332e1b228cd0ac18785dbc30aa4c8e3594e7081f
anish2500 <tmganish2500@gmail.com>
2025-01-26 11:50:55 +0545

    sprint 6

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,22 +1,52 @@
 
+import './App.css'
+
 import React from 'react';
+import SignUp from "./components/SignUp";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BrowsePage from './components/BrowsePage'; 
 import Profile from './components/Profile'; 
 import CreateBlog from './components/CreateBlog';
-import Login from "./components/Login"
-
+import Login from './components/Login';
 function App() {
   return (
     <Router>
+      <div className="app-container">
         <Routes>
-          <Route path="/login" element={<Login/>}/>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
+          
           <Route path="/" element={<BrowsePage />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/createBlog" element={<CreateBlog />} />
+          
         </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
+
+
+
+
+// import React from "react";
+// import SignUp from "./components/SignUp";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <SignUp />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+
