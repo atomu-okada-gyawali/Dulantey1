ef545584fc9b4df8cbce5158a16562bbd1ef4566
vinayakstha <vsth005@gmail.com>
2025-02-02 19:16:53 +0545

    blog-controller

--- backend/routes/blogs.routes.js
+++ backend/routes/blogs.routes.js
@@ -4,5 +4,10 @@ import blogController from "../controller/blog.controller.js";
 
 // router.get("/registration", );
 router.post("/createBlog", blogController.create);
+router.put("/updateBlog/:id", blogController.update);
+router.delete("/deleteBlog/:id", blogController.delete);
+router.get("/getAllBlogs", blogController.getAllBlogs);
+router.get("/getBlogById/:id", blogController.getBlogsById);
+
 
 export default router;
